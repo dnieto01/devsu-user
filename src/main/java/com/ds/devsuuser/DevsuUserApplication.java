12fc6c1ba0a0e4289d448501af91f35d1874012f
Dnieto <dnieto@Dnietos-MacBook-Pro.local>
2026-03-17 12:02:21 -0500

    add project

---
+++ src/main/java/com/ds/devsuuser/DevsuUserApplication.java
@@ -0,0 +1,13 @@
+package com.ds.devsuuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsuUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsuUserApplication.class, args);
+	}
+
+}
