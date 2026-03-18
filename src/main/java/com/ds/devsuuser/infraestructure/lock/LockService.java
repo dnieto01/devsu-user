d26dc4614373dbf1bf1a52cdfd695b354ca659f1
Dnieto <dnieto@Dnietos-MacBook-Pro.local>
2026-03-18 00:00:38 -0500

    add lock service

--- src/main/java/com/ds/devsuuser/infraestructure/lock/LockService.java
+++ src/main/java/com/ds/devsuuser/infraestructure/lock/LockService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPooled;
 import redis.clients.jedis.params.SetParams;
 
 import java.util.UUID;
--- src/main/java/com/ds/devsuuser/infraestructure/lock/LockServiceLocal.java
+++ src/main/java/com/ds/devsuuser/infraestructure/lock/LockServiceLocal.java
@@ -3,13 +3,8 @@ package com.ds.devsuuser.infraestructure.lock;
 import com.ds.devsuuser.infraestructure.exceptions.ApiException;
 import com.ds.devsuuser.infraestructure.exceptions.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import redis.clients.jedis.JedisPooled;
-import redis.clients.jedis.params.SetParams;
 
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -22,10 +17,8 @@ public class LockServiceLocal implements ILockService {
 
     private final ConcurrentMap<String, String> localLocks = new ConcurrentHashMap<>();
 
-
     @Override
     public boolean acquireLock(String key) {
-
         isLocked(key);
 
         String uuid = UUID.randomUUID().toString();
